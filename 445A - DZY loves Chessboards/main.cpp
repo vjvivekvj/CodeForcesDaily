173859559d68e267cd6ec2f0263c294810287a20
Vivek Joshi <vivekj.joshi9@gmail.com>
2021-06-03 15:49:44 +0530

    cpp soln of 58A Chat Room

---
+++ 445A - DZY loves Chessboards/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#define FOR(i,a,b) for(int i=a; i<b; i++)
+using namespace std;
+
+int main()
+{
+    int r,c ;
+    cin>>r>>c ;
+    int chessboard[r][c] ;
+    FOR(i,0,r){
+        FOR(j,0,c){
+
+        }
+    }
+    return 0;
+}
