bef64fd0ee41584d8fb991cdd49163499864a614
Vivek Joshi <vivekj.joshi9@gmail.com>
2021-09-13 16:42:06 +0530

    cpp soln of 1566A

---
+++ 1566A/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <unordered_map>
+#define FOR(i,a,b) for(int i=a; i<b; i++)
+#define debug(x) cout << #x << " " << x <<endl
+using namespace std;
+
+
+
+void solve(int t){
+    int n,s ;
+    cin>>n>>s;
+    int eqEl = n/2 + 1;
+
+    cout<<s/eqEl<<endl ;
+}
+
+
+int main() {
+	int t ;
+	cin>>t ;
+	FOR(i,1,t+1){
+	    solve(i) ;
+	}
+	return 0;
+}
